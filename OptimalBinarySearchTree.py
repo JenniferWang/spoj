30580b19531117d18faf52827e37c6395b28ab1f
Jennifer Jiyue Wang <jiyue@stanford.edu>
2014-11-29 11:48:20 -0800

    modified, following Yunzhi's suggestions.

--- OptimalBinarySearchTree.py
+++ OptimalBinarySearchTree.py
@@ -1,22 +1,26 @@
 def optCost(freqs):
+  """
+  http://www.geeksforgeeks.org/dynamic-programming-set-24-optimal-binary-search-tree/
+  cost definitin is calcuated according to 
+  http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1245
+  """
   n = len(freqs)
-  max_freq = max(freqs)
-  cost = [[None for y in range(n)]for x in range(n)]
-  for x in range(n):
-    cost[x][x] = 0
+  cost = [[None for y in xrange(n)] for x in xrange(n)]
+  for start in xrange(n):
+    cost[start][start] = 0
 
-  for length in range(2, n + 1):
-    for x in range(n - length + 1):
-      summation = sum(freqs[x: x + length])
-      cost[x][x + length - 1] = min(
-        summation - freqs[x] + cost[x + 1][x + length - 1],
-        summation - freqs[x + length - 1] + cost[x][x + length - 2]
+  for length in xrange(2, n + 1):
+    for start in xrange(n - length + 1):
+      summation = sum(freqs[start: start + length])
+      cost[start][start + length - 1] = min(
+        summation - freqs[start] + cost[start + 1][start + length - 1],
+        summation - freqs[start + length - 1] + cost[start][start + length - 2]
       ) 
       
-      for k in range(x + 1, x + length - 1):
-        cost[x][x + length - 1] = min(
-          cost[x][x + length - 1],
-          cost[x][k - 1] + cost[k + 1][x + length - 1] + summation - freqs[k]
+      for k in xrange(start + 1, start + length - 1):
+        cost[start][start + length - 1] = min(
+          cost[start][start + length - 1],
+          cost[start][k - 1] + cost[k + 1][start + length - 1] + summation - freqs[k]
         )
 
   return cost[0][n - 1]
@@ -32,7 +36,7 @@ def main():
   for i in range(len(s)):
     line = s[i].split()
     freqs = line[1:]
-    freqs = [int(x) for x in freqs]
+    freqs = map(int, freqs) # Attention
     print optCost(freqs)
 
 if __name__=='__main__':

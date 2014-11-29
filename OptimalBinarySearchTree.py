81c8380a0bf0f1e4af4c84755be47c44641bb924
Jennifer Jiyue Wang <jiyue@stanford.edu>
2014-11-29 11:22:34 -0800

    ...

---
+++ OptimalBinarySearchTree.py
@@ -0,0 +1,38 @@
+def optCost(freqs):
+  n = len(freqs)
+  max_freq = max(freqs)
+  cost = [[None for y in range(n)]for x in range(n)]
+  for x in range(n):
+    cost[x][x] = 0
+
+  for length in range(2, n + 1):
+    for x in range(n - length + 1):
+      summation = sum(freqs[x: x + length])
+      cost[x][x + length - 1] = min(
+        summation - freqs[x] + cost[x + 1][x + length - 1],
+        summation - freqs[x + length - 1] + cost[x][x + length - 2]
+      ) 
+      
+      for k in range(x + 1, x + length - 1):
+        cost[x][x + length - 1] = min(
+          cost[x][x + length - 1],
+          cost[x][k - 1] + cost[k + 1][x + length - 1] + summation - freqs[k]
+        )
+
+  return cost[0][n - 1]
+
+def main():
+  #g=sys.stdin
+  g = open("OptimalBST", "r") 
+  s=g.read().splitlines()
+  while '' in s: s.remove('')
+  while '\n' in s: s.remove('\n')
+  
+  for i in range(len(s)):
+    line = s[i].split()
+    freqs = line[1:]
+    freqs = [int(x) for x in freqs]
+    print optCost(freqs)
+
+if __name__=='__main__':
+  main()

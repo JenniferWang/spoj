f9f3b3c9d06dca6c92218db8b17b0d827f72fe77
Jennifer Jiyue Wang <jiyue@stanford.edu>
2014-12-05 08:14:01 -0800

    修改过的版本

--- 14834Foxlings.py
+++ 14834Foxlings.py
@@ -2,15 +2,13 @@
 # http://www.spoj.com/problems/FOXLINGS/
 class DisjointSet():
   def __init__(self, n):
-    self.data = [x for x in range(n)]
-    self.id = [x for x in range(n)]
+    self.root = [x for x in xrange(n)]
     self.num = n # number of disjoint sets
-    self.sz = [1 for x in range(n)]
-    self.n = n
+    self.sz = [1 for x in xrange(n)]
   def findRoot(self, val):
-    while val != self.id[val]:
-      self.id[val] = self.id[self.id[val]]
-      val = self.id[val]
+    while val != self.root[val]:
+      self.root[val] = self.root[self.root[val]]
+      val = self.root[val]
     return val
   def union(self, val1, val2):
     val1 = self.findRoot(val1)
@@ -18,10 +16,10 @@ class DisjointSet():
     if val1 == val2:
       return
     if self.sz[val1] > self.sz[val2]:
-      self.id[val2] = val1
+      self.root[val2] = val1
       self.sz[val1] += self.sz[val2]
     else:
-      self.id[val1] = val2
+      self.root[val1] = val2
       self.sz[val2] += self.sz[val1]
     self.num -= 1
 
@@ -40,14 +38,11 @@ def main():
   l1 = s[0].split( )
   num_nodes = int(l1[0])
   num_edges = int(l1[1])
-  edges = []
-  for edge in range(1, num_edges + 1):
-    l = s[edge].split( )
-    edges.append((int(l[0]) - 1, int(l[1]) - 1))
-
   djSet = DisjointSet(num_nodes)
-  for val1, val2 in edges:
-    djSet.union(val1, val2)
+  for edge in xrange(1, num_edges + 1):
+    l = s[edge].split( )
+    djSet.union(int(l[0]) - 1, int(l[1]) - 1)
+
   print djSet.num
    
 if __name__ == '__main__':
